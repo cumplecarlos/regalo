2be671616227c412b05a57199bd38136d00917e0
Nadia Faya <nadia.faya@booking.com>
2017-07-04 23:22:52 +0200

    Completed implementation of game

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var car = document.querySelector("#car");
 var finishLine = document.querySelector("#finish-line");
+var grass = document.querySelector("#grass");
+var shortcutMessage = document.querySelector("#short-cut-message");
+var grass2 = document.querySelector("#grass-2");
+var shortcutMessage2 = document.querySelector("#short-cut-message-2");
 var hiddenMessage = document.querySelector("#hidden-message");
 var closeHiddenMessageButton = document.querySelector("#close-hidden-message");
 var movementLength= 10;
@@ -12,45 +16,90 @@ var opositeDirection = {
 	Y: 'X'
 };
 var finishPosition = {
-	X: finishLine.getBoundingClientRect().left + 30,
-	Y: finishLine.getBoundingClientRect().top + 20
+	left: finishLine.getBoundingClientRect().left + 30,
+	bottom: finishLine.getBoundingClientRect().bottom - 30,
+	top: finishLine.getBoundingClientRect().top + 20
 }
-function checkIfWonRace() {
-	if (car.getBoundingClientRect().bottom > finishPosition.Y && car.getBoundingClientRect().right > finishPosition.X) {
-		hiddenMessage.style.display = 'block';
+var grassPosition = {
+	X: grass.getBoundingClientRect().left + 30,
+	Y: grass.getBoundingClientRect().bottom - 20
+}
+var grassPosition2 = {
+	Y: grass2.getBoundingClientRect().top + 20
+}
+var orientationDegree = {
+	'right': 0,
+	'down': 90,
+	'left': 180,
+	'up': 270
+};
+var currentOrientation = 'right';
+var showedHiddenMessage = false;
+
+function checkIfStepOnGrass() {
+	if (car.getBoundingClientRect().top < grassPosition.Y && car.getBoundingClientRect().right > grassPosition.X) {
+		shortcutMessage.style.display = 'block';
+	} else {
+		shortcutMessage.style.display = 'none';
 	}
 }
 
-function move(direction, initialPosition, finalPosition) {
+function checkIfStepOnGrass2() {
+	if (car.getBoundingClientRect().bottom > grassPosition2.Y) {
+		shortcutMessage2.style.display = 'block';
+	} else {
+		shortcutMessage2.style.display = 'none';
+	}
+}
+
+function checkIfWonRace() {
+	var carPosition = car.getBoundingClientRect();
+	if (carPosition.bottom > finishPosition.top && carPosition.top < finishPosition.bottom && carPosition.right > finishPosition.left) {
+		if (!showedHiddenMessage) {
+			hiddenMessage.style.display = 'block';
+			showedHiddenMessage = true;
+		}
+	} else {
+		showedHiddenMessage = false;
+	}
+}
+
+function move(orientation, direction, initialPosition, finalPosition) {
+	var previousOrientation = currentOrientation;
+	currentOrientation = orientation;
 	var animation = car.animate([
-		{ transform: 'translate' + direction + '(' + initialPosition  + 'px) translate'+ opositeDirection[direction] +'(' + position[opositeDirection[direction]]+'px)' },
-		{ transform: 'translate' + direction + '(' + finalPosition + 'px) translate'+ opositeDirection[direction] +'('+position[opositeDirection[direction]]+'px)' }
+		{ transform: 'translate' + direction + '(' + initialPosition  + 'px) translate'+ opositeDirection[direction] +'(' + position[opositeDirection[direction]]+'px) rotate(' + orientationDegree[previousOrientation] + 'deg)' },
+		{ transform: 'translate' + direction + '(' + finalPosition + 'px) translate'+ opositeDirection[direction] +'('+position[opositeDirection[direction]]+'px) rotate(' + orientationDegree[orientation] + 'deg)' }
 	], { duration: 100, fill: 'forwards' });
-	animation.onfinish = checkIfWonRace;
+	animation.onfinish = function() {
+		checkIfStepOnGrass();
+		checkIfStepOnGrass2();
+		checkIfWonRace();
+	};
 }
 
 function moveRight() {
 	var initialPosition = position.X;
 	position.X += movementLength;
-	move('X', initialPosition, position.X);
+	move('right', 'X', initialPosition, position.X);
 }
 
 function moveLeft() {
 	var initialPosition = position.X;
 	position.X -= movementLength;
-	move('X', initialPosition, position.X);	
+	move('left' ,'X', initialPosition, position.X);	
 }
 
 function moveUp() {
 	var initialPosition = position.Y;
 	position.Y -= movementLength;
-	move('Y', initialPosition, position.Y);
+	move('up' ,'Y', initialPosition, position.Y);
 }
 
 function moveDown() {
 	var initialPosition = position.Y;
 	position.Y += movementLength;
-	move('Y', initialPosition, position.Y);
+	move('down' ,'Y', initialPosition, position.Y);
 }
 
 function closeHiddenMessage() {
